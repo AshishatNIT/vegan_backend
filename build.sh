610fcbe42048c6d3beccf5ae5f018977cbbb3d48
Ashish Singh <ashish1qwerty1@gmail.com>
2025-12-28 22:40:32 +0530

    Add WhiteNoise middleware for static file handling and update build script

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 # exit on error
 set -o errexit
 
+python manage.py collectstatic --no-input
+
 python manage.py migrate
--- config/settings.py
+++ config/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # <-- ADD THIS LINE
     'django.middleware.common.CommonMiddleware',
@@ -128,6 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# This tells Django where to gather all static files (CSS, JS) during deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# This enables WhiteNoise to compress and cache files for speed
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
